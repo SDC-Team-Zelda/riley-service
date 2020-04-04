d48ee1331bffe92f5361b55d3ecdee2c3a3259ce
rlwong2 <rlwong2@gmail.com>
2020-04-04 15:21:08 -0700

    Created CRUD for Postgres db

---
+++ database-Postgres/index.js
@@ -0,0 +1,74 @@
+const { Pool } = require('pg')
+
+const pool = new Pool({
+  host: 'localhost',
+  database: 'sdc',
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+})
+
+let getListing = async function(id, callback) {
+  try {
+    const res = await pool.query(`SELECT * FROM listings WHERE id = ${id}`)
+    console.log(res.rows[0])
+    callback(null, res.rows[0])
+  } catch(err) {
+    console.log(err.stack)
+    callback(err.stack, null)
+  }
+}
+
+let postListing = async function(object, callback) {
+  try {
+    console.log('POST: ', object)
+    const text = 'INSERT INTO listings(title, description, photos) VALUES($1, $2, $3) RETURNING *';
+    const values = Object.values(object)
+
+    const res = await pool.query(text, values)
+    console.log(res.rows[0])
+    callback(null, res.rows[0])
+  } catch(err) {
+    console.log(err.stack)
+    callback(err.stack, null)
+  }
+}
+
+let putListing = async function(id, object, callback) {
+  try {
+
+    let values = [];
+    let keys = Object.keys(object);
+    for (var key in object) {
+      values.push(object[key])
+    }
+    // values = values.slice(0, -2);
+
+    const text = `UPDATE listings SET description = $1 WHERE id = ${id} RETURNING *`;
+
+    const res = await pool.query(text, values)
+    console.log(res.rows[0])
+    callback(null, res.rows[0])
+  } catch(err) {
+    console.log(err.stack)
+    callback(err.stack, null)
+  }
+}
+
+let deleteListing = async function(id, callback) {
+  try {
+    const res = await pool.query(`DELETE FROM listings WHERE id = ${id}`)
+    console.log(res.rows[0])
+    callback(null, res.rows[0])
+  } catch(err) {
+    console.log(err.stack)
+    callback(err.stack, null)
+  }
+}
+
+module.exports = {
+  getListing: getListing,
+  postListing: postListing,
+  putListing: putListing,
+  deleteListing: deleteListing
+};
