9d91bdfaecad6b1df9181e60c8f9260f062c8597
rlwong2 <rlwong2@gmail.com>
2020-04-08 15:55:10 -0700

    Fixed proxy not loading pictures.  Using array data-type in postgres

--- dataGen/dataGen.js
+++ dataGen/dataGen.js
@@ -2,20 +2,14 @@ const faker = require('faker');
 const fs = require('fs');
 const perf = require('execution-time')();
 
-const uuid  = require('uuid');
+const fileName = 'list';
 
-
-const writeUsers = fs.createWriteStream('./dataGen/data/list_no_UUID.tsv')
-
-const uuid  = require('uuid');
-
+const writeUsers = fs.createWriteStream(`./dataGen/data/${fileName}.tsv`)
 writeUsers.write('id\ttitle\tdescription\tphotos\n', 'utf8');  // write headers
-// writeUsers.write('uuid\ttitle\tdescription\tphotos\n', 'utf8');  // write headers
 
 function writeTenMillionUsers(writer, encoding, callback) {
-  // perf.start();
 
-  let i = 10000000;
+  let i = 10;
   let j = 0;
   let onePercent = i / 100;
   function write() {
@@ -29,18 +23,17 @@ function writeTenMillionUsers(writer, encoding, callback) {
       }
 
       // const uniqueId = uuid.v4();
-      const title = faker.lorem.sentence();
+      const title = faker.lorem.words();
       const description = faker.lorem.sentence() + ' ' + faker.lorem.sentence();
       const photosArr = [];
       const p = Math.floor(Math.random() * 5) + 1;
       for (var k = 0; k < p; k++ ) {
         let padded = (Math.ceil(Math.random() * 1500)).toString().padStart(4, '0');
-        photosArr.push(`/photo-${padded}.jpg`)
+        photosArr.push(`"https://sdc-jpgs.s3.us-east-2.amazonaws.com/photos/photo-${padded}.jpg"`)
       }
 
-      // const photos = JSON.stringify(photosArr)
-      // const data = `${uniqueId}\t${title}\t${description}\t${photos}\n`;
-      const data = `${j}\t${title}\t${description}\t${photosArr}\n`;
+      const photos = '{' + photosArr.join(',') + '}';
+      const data = `${j}\t${title}\t${description}\t${photos}\n`;
 
       if (i === 0) {
         writer.write(data, encoding, callback);
@@ -55,19 +48,15 @@ function writeTenMillionUsers(writer, encoding, callback) {
 // write some more once it drains
       writer.once('drain', write);
     }
-    // const results = perf.stop();
-    // console.log(results.time)
   }
 write()
 
 }
 
 
-
-
 writeTenMillionUsers(writeUsers, 'utf-8', () => {
   console.log('Finished data generation')
-  var stats = fs.statSync('./dataGen/data/list.tsv');
+  var stats = fs.statSync(`./dataGen/data/${fileName}.tsv`);
   var fileSizeB = stats["size"];
   var fileSizeMB = fileSizeB / 1000000.0
 
