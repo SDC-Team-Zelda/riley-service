f16f20624114eb40a6c39426c446670881d600b5
rlwong2 <rlwong2@gmail.com>
2020-04-08 11:03:11 -0700

    Fixing dupes

--- webpack.prod.js
+++
@@ -1,8 +0,0 @@
-const merge = require('webpack-merge');
-const config = require('./webpack.config.js');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
-module.exports = merge(config, {
-  mode: 'production',
-  plugins: [new CleanWebpackPlugin()]
-});
