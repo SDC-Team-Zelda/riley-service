e9d05698c0c0b8ddaccefd621c09156a6c84f5ad
rlwong2 <rlwong2@gmail.com>
2020-04-16 19:33:37 -0700

    Added compression to express server and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,18 @@ var path = require('path');
 var SRC_DIR = path.join(__dirname, '/client/src');
 var DIST_DIR = path.join(__dirname, 'client/public/dist');
 const TerserPlugin = require('terser-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 // const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'production',
   entry: `./client/src/Index.js`,
   output: {
     filename: 'app.js',
     path: DIST_DIR
   },
+  plugins: [new CompressionPlugin()],
   module : {
     rules : [
       {
